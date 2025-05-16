cc20d21619b4996c986353f2bed7dbcd414f89a0
minseong7164 <minseong7164@naver.com>
2025-05-16 17:23:28 +0900

    feat :springboot pratice

--- src/main/java/com/korit/springboot_basic/controller/BasicController.java
+++ src/main/java/com/korit/springboot_basic/controller/BasicController.java
@@ -1,4 +1,45 @@
 package com.korit.springboot_basic.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
 public class BasicController {
+
+    //MediaType.TEXT_HTML_VALUE
+    // "text/html"  10번라인 과 11번 라인은 동일하다.
+
+    @GetMapping(value = "/index",produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String index() {
+        List<String> names = List.of("김준일", "김준이", "김준삼");
+        return """
+                <html>
+                <head></head>
+                <body>
+                <h1>제목</h1>
+                """
+                +
+                names.stream()
+                        .map(name -> "<div>" + name + "</div>")
+                        .collect(Collectors.joining())
+                +
+                """
+                <body>
+                </html>
+                """;
+    }
+
+    @GetMapping("/index2")
+    public String index2(Model model) {
+        List<String> names = List.of("김준일", "김준이", "김준삼");
+        model.addAttribute("names", names);
+        return "index";
+    }
 }
--- src/main/java/com/korit/springboot_basic/controller/DataController.java
+++ src/main/java/com/korit/springboot_basic/controller/DataController.java
@@ -1,4 +1,24 @@
 package com.korit.springboot_basic.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
 public class DataController {
+
+    @GetMapping("/api/names")
+    public ResponseEntity<List<String>> getNames() {
+        List<String> names = List.of("김준일", "김준이", "김준삼");
+        return ResponseEntity.ok(names);
+    }
+    @GetMapping("/api/addresses")
+    public ResponseEntity<List<String>> getAddresses() {
+        List<String> addresses = List.of("부산진구", "동래구", "북구", "남구", "서구");
+        return ResponseEntity.ok(addresses);
+    }
 }
